e0c6ebe55df7c61186d7d31c072504c265759c35
YaniEmanuel <116165793+YaniEmanuel@users.noreply.github.com>
2024-07-30 09:56:22 -0300

    Create Product.js

---
+++ backend/src/models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  sku: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
